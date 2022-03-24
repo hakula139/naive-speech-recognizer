5a45f9de8b81afa4a6d828ae12eef4cfc9dffc74
Hakula Chen <i@hakula.xyz>
2022-03-25 00:25:32 +0800

    feat: utilize numpy's implementation of fft as a reference

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
+import numpy.fft as nf
 import librosa
 
-from utils import plot_time_domain
+from utils import *
 
 
 # Parameters
 wav_path = 'data/signal.wav'
 fig_time_path = 'assets/time_domain.png'
+fig_freq_path = 'assets/freq_domain.png'
 sample_rate = 8000.
 n_samples = 1024
 
@@ -19,6 +21,13 @@ def main() -> None:
     t0 = np.arange(0, n_samples, 1) / sr
     y0 = y[:n_samples]
     plot_time_domain(fig_time_path, t0, y0)
+
+    # Do FFT.
+    y0_freqs = nf.fftfreq(n_samples, 1 / sr)
+    y0_fft = np.abs(nf.fft(y0))
+    plot_freq_domain(
+        fig_freq_path, y0_freqs[y0_freqs >= 0],  y0_fft[y0_freqs >= 0],
+    )
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -12,10 +12,30 @@ def plot_time_domain(output_path: str, t: np.ndarray, y: np.ndarray) -> None:
         `y`: amplitudes of samples
     '''
 
+    plt.figure()
     plt.title('Time Domain')
     plt.xlabel('Time / s')
     plt.ylabel('Amplitude')
-    plt.plot(t, y, label='signal')
+    plt.plot(t, y, c='blue', label='signal')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(output_path)
+
+def plot_freq_domain(output_path: str, f: np.ndarray, y: np.ndarray) -> None:
+    '''
+    Plot the amplitude spectrum of a wave in frequency domain.
+
+    Args:
+        `output_path`: path to the output figure
+        `f`: frequency range
+        `y`: amplitude spectrum
+    '''
+
+    plt.figure()
+    plt.title('Frequency Domain')
+    plt.xlabel('Frequency / Hz')
+    plt.ylabel('Amplitude')
+    plt.plot(f, y, c='red', label='power')
     plt.legend()
     plt.tight_layout()
     plt.savefig(output_path)
